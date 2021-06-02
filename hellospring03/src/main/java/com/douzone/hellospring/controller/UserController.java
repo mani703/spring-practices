2decc0d7ca173a24b57d65aadf1d948271fe6af7
BIT <mani703@naver.com>
2021-06-02 16:00:13 +0900

    @RequestMapping 연습

---
+++ hellospring03/src/main/java/com/douzone/hellospring/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.douzone.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @RequestMapping
+ * 클래스 + 핸들러(메소드)
+ * @author BIT
+ *
+ */
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@RequestMapping(value="/join", method=RequestMethod.GET)
+	public String join() {
+		return "/WEB-INF/views/join.jsp";
+	}
+	
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(UserVo vo) {
+		System.out.println(vo);
+		return "redirect:/";
+	}
+}
