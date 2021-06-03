d8120b89a065c7463aabb5039a000d41010b7a34
BIT <mani703@naver.com>
2021-06-03 11:02:47 +0900

    emaillist #03

--- hellospring03/src/main/java/com/douzone/hellospring/controller/UserController.java
+++ hellospring03/src/main/java/com/douzone/hellospring/controller/UserController.java
@@ -3,6 +3,8 @@ package com.douzone.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @RequestMapping
@@ -24,5 +26,38 @@ public class UserController {
 	public String join(UserVo vo) {
 		System.out.println(vo);
 		return "redirect:/";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/update")
+	public String update(@RequestParam("n") String name) {
+		/**
+		 * 만일 n이라는 이름의 파라미터가 없는 경우
+		 * 400 Bad Request 에러가 발생한다
+		 */
+		System.out.println(name);
+		return "UserController.update()";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/update2")
+	public String update2(
+			@RequestParam(value="n", required=true, defaultValue="") String name,
+			@RequestParam(value="a", required=true, defaultValue="0") int age) {
+		System.out.println("---" + name + "---" + age);
+		if(name == null) {
+			name = "";
+		}
+		return "UserController.update2()";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/update3")
+	public String update2(@RequestParam(value="n", required=false) String name){
+		System.out.println("---" + name);
+		if(name == null) {
+			name = "";
+		}
+		return "UserController.update3()";
 	}
 }
