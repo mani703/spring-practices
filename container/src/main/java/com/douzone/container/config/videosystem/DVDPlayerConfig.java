6a50489673b0f3d1eb4d0126a4506d187e61ec36
BIT <mani703@naver.com>
2021-06-14 11:45:27 +0900

    explicit wiring #java - 1

---
+++ container/src/main/java/com/douzone/container/config/videosystem/DVDPlayerConfig.java
@@ -0,0 +1,14 @@
+package com.douzone.container.config.videosystem;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.douzone.container.videosystem.Avengers;
+
+@Configuration
+public class DVDPlayerConfig {
+	@Bean
+	public Avengers avenger() {
+		return new Avengers();
+	}
+}
